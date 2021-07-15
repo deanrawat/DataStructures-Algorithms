2aa450f4f3569b124410bc97039c7bdbba7eca2f
ankit singh rawat <ankitsinghrawat37@gmail.com>
2021-07-15 22:24:26 +0530

    problem 1051 added

---
+++ leetcode.com/1051. Height Checker.cpp
@@ -0,0 +1,33 @@
+// First go to leetcode.com -> problems -> search 1051
+/*
+A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
+
+You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+
+Return the number of indices where heights[i] != expected[i].
+
+Example 1:
+
+Input: heights = [1,1,4,2,1,3]
+Output: 3
+Explanation: 
+heights:  [1,1,4,2,1,3]
+expected: [1,1,1,2,3,4]
+Indices 2, 4, and 5 do not match.
+
+*/
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> v;
+        v = heights;
+        sort(v.begin(),v.end());
+        int count = 0;
+        for(int i = 0; i < heights.size(); i++){
+            if(heights[i] != v[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+};
