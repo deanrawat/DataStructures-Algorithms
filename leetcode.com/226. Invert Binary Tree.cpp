8a784aff304923b6549ecc34d34eeba349c1c166
ankit singh rawat <ankitsinghrawat37@gmail.com>
2021-07-21 14:32:20 +0530

    prob 226 added

--- leetcode.com/226. Invert Binary Tree.cpp
+++ leetcode.com/226. Invert Binary Tree.cpp
@@ -38,9 +38,7 @@ public:
         if(root == NULL){
             return root;
         }
-        TreeNode* temp = root->left;
-        root->left = root->right;
-        root->right = temp;
+        swap(root->left,root->right);
         invertTree(root->left);
         invertTree(root->right);
         return root;
